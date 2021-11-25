b7be5db08988d003723c89a5e271cd30f75a4e1f
MonishaG09 <94268410+MonishaG09@users.noreply.github.com>
2021-11-25 09:44:26 +0530

    Update test.c

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -1,1 +1,22 @@
+#include "unity.h"
+#include <Phonebook.h>
 
+/* Modify these two lines according to the project */
+#include <phonebook.h>
+#define PROJECT_NAME    "Phonebook"
+
+/* Prototypes for all the test functions */
+void test_Name(void);
+void test_Address(void);
+void test_Sex(void);
+void test_Bloodgroup(void);
+void test_DOB(void);
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Start of the application test */
+int main()
+{
