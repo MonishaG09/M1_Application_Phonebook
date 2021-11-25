b55b76d4d6c5040e60d5979374d93a74c7f46c22
MonishaG09 <94268410+MonishaG09@users.noreply.github.com>
2021-11-25 09:46:22 +0530

    Update test.c

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -20,3 +20,16 @@ void tearDown(){}
 /* Start of the application test */
 int main()
 {
+  /* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_Name);
+  RUN_TEST(test_Address);
+  RUN_TEST(test_sex);
+  RUN_TEST(test_Bloodgroup);
+   RUN_TEST(test_DOB);
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
